65aef2db72ca0b13fbddf09ac979cb8ff555f957
ElGibran17 <ibrahimelgibran17@gmail.com>
2025-05-06 12:10:57 +0700

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+
+<footer class="footer">
+	<div class="d-sm-flex justify-content-center justify-content-sm-between">
+		<span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Made with love by <a href="https://iegcode.com" target="_blank">Iegcode.</span>
+        <span class="text-muted float-none float-sm-right d-block mt-1 mt-sm-0 text-center pr-3">Presentration by:  Iegcode</span>
+    </div>
+</footer>
